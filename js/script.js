8030d46ce126a12aad15a167591f0a829f1d8f0b
ateBitDev <raul.santiago.aguirre@gmail.com>
2018-11-29 20:28:53 -0800

    almost done

--- js/script.js
+++ js/script.js
@@ -1,52 +1,62 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
+const page = document.querySelector('.page');
+const list = document.querySelector('ul');
+const studentList = list.children;
+const numOfPages = (studentList.length/10);
+//takes in a list of students and page number
+const  showPage = (list,page) =>
+{
+  for (let i = 0; i < list.length; i += 1)
+  {
+    //if index is within page number intervals shows  up on screen
+      if (i >= ((page*10)-10) && i < (page*10))
+      {
+          list[i].style.display = 'block';
+      }
+      else
+      {
+          list[i].style.display = 'none';
+      }
+  }
+}
+//takes in a list of stduents and creates a tag buttons
+const appendPageLinks = (list) =>
+{
+  let paginationDiv = document.createElement('div');
+  const ul = document.createElement('ul');
+  paginationDiv.className = 'pagination';
+  page.appendChild(paginationDiv);
+  paginationDiv.appendChild(ul)
 
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+//creates tags with page numbers
+  for (let i = 0; i < numOfPages; i += 1)
+  {
 
+    const li = document.createElement('li');
+    const  a = document.createElement('a');
 
-/***
-   Add your global variables that store the DOM elements you will
-   need to reference and/or manipulate.
-   But be mindful of which variables should be global and which
-   should be locally scoped to one of the two main functions you're
-   going to create. A good general rule of thumb is if the variable
-   will only be used inside of a function, then it can be locally
-   scoped to that function.
-***/
-let list = document.querySelectorAll('ul')[0];
-list.style.backgroundColor = 'blue'
+    a.textContent = ((i+1).toString());
 
+    ul.appendChild(li);
+    li.appendChild(a);
 
+    a.addEventListener('click', (e) => {
+      let page = e.target;
+      let aTags = document.querySelectorAll('a');
+//highlights current page tag
+      for (let i = 0; i < aTags.length;i += 1)
+      {
+        if(aTags[i] === page)
+        aTags[i].style.backgroundColor = 'highlight';
+        else {
+        aTags[i].style.backgroundColor = '';
+        }
+      }
 
-
-
-/***
-   Create the `showPage` function to hide all of the items in the
-   list except for the ten you want to show.
-
-   Pro Tips:
-     - Keep in mind that with a list of 54 students, the last page
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when
-       you initially define the function, and it acts as a variable
-       or a placeholder to represent the actual function `argument`
-       that will be passed into the parens later when you call or
-       "invoke" the function
-***/
-
-
-
-
-/***
-   Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
-***/
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+      showPage(studentList,parseInt(page.textContent));
+});
+}
+};
+//initail call of func to create a tags
+appendPageLinks(studentList);
+//called so page loads only first 10 students
+showPage(studentList,1);
