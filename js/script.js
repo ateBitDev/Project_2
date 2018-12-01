99870960504bdcead5204d331038b65a180f0af8
ateBitDev <raul.santiago.aguirre@gmail.com>
2018-11-30 18:34:23 -0800

    last changes
    mostly for inital start of file

--- js/script.js
+++ js/script.js
@@ -18,45 +18,55 @@ const  showPage = (list,page) =>
       }
   }
 }
-//takes in a list of stduents and creates a tag buttons
+//takes in a list of students and creates a tag buttons
 const appendPageLinks = (list) =>
 {
-  let paginationDiv = document.createElement('div');
+  const paginationDiv = document.createElement('div');
   const ul = document.createElement('ul');
+
   paginationDiv.className = 'pagination';
   page.appendChild(paginationDiv);
-  paginationDiv.appendChild(ul)
+  paginationDiv.appendChild(ul);
 
 //creates tags with page numbers
   for (let i = 0; i < numOfPages; i += 1)
   {
-
     const li = document.createElement('li');
     const  a = document.createElement('a');
 
-    a.textContent = ((i+1).toString());
+    a.textContent = (i + 1);
 
     ul.appendChild(li);
     li.appendChild(a);
 
+    if (a.textContent === '1')
+    //starts page off with first atag highlighted
+    a.style.backgroundColor = 'highlight';
+
     a.addEventListener('click', (e) => {
+
+      const aTags = document.querySelectorAll('a');
       let page = e.target;
-      let aTags = document.querySelectorAll('a');
-//highlights current page tag
-      for (let i = 0; i < aTags.length;i += 1)
+      page.className = 'active';
+
+      //highlights current page tag
+      for (let i = 0; i < aTags.length; i += 1)
       {
-        if(aTags[i] === page)
-        aTags[i].style.backgroundColor = 'highlight';
-        else {
-        aTags[i].style.backgroundColor = '';
+        if (aTags[i] === page)
+        {
+        page.style.backgroundColor = 'highlight';
+        }
+        else
+        {
+          aTags[i].style.backgroundColor = '';
+          aTags[i].className = '';
         }
       }
-
-      showPage(studentList,parseInt(page.textContent));
+      showPage(studentList,page.textContent);
 });
 }
 };
-//initail call of func to create a tags
+//initial call of func to show page '1' first
+showPage(studentList,1)
+//initial call of func to create a tags
 appendPageLinks(studentList);
-//called so page loads only first 10 students
-showPage(studentList,1);
